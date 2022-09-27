6ec54d60f0506a0a6e4fea2d63ef73df706ceb6e
mkfuentes <miguelangel.fm@outlook.com>
2022-09-26 22:42:41 -0500

    first scss

--- landing/src/App.js
+++ landing/src/App.js
@@ -1,7 +1,8 @@
 import './App.scss'
 import {FaBehance, FaDribbble} from 'react-icons/fa'
 import { IoMailOutline, IoChevronForwardCircle, IoStar } from 'react-icons/io5'
-import {IconContext} from 'react-icons'
+import { IconContext } from 'react-icons'
+import Card from '../src/components/Card'
 
 function App() {
   return (
@@ -11,13 +12,13 @@ function App() {
       <div className="menu_container"></div>
       <span>
         <IconContext.Provider value={{color:"#000", size:"18px", className:"icons_container"}}>
-          <div className="icon">FaBehance</div>
-          <div className="icon">FaDribble</div>
+          <div className="icon"><FaBehance/></div>
+          <div className="icon"><FaDribbble/></div>
         </IconContext.Provider>
       </span>
       <span>
         <IconContext.Provider value={{ color: "#000", size: "18px" }}>
-          <div className="icon">IoMailOutline</div>
+          <div className="icon"><IoMailOutline/></div>
           hello@example.com
         </IconContext.Provider>
       </span>
@@ -31,8 +32,8 @@ function App() {
     <div className="content_wrapper">
         <div className="left_content_wrapper">
           <h2>
-            <span>Design focused</span>
-            <span>start up</span>
+            <span>Design <span className="second"> focused</span></span>
+            <span>start <span className="second">up</span></span>
           </h2>
           <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Inventore voluptate, molestiae quod, provident nemo reprehenderit explicabo est dicta iste nesciunt natus numquam velit beatae exercitationem dolor esse itaque suscipit assumenda?</p>
           <div className="btn_group">
@@ -52,15 +53,16 @@ function App() {
               <span><IoStar/></span>
               <span><IoStar/></span>
             </IconContext.Provider>
-            <p>More than 50 people</p>
+            <p className="review_containe">More than 50 people</p>
           </div>
 
         </div>
         <div className="right_content_wrapper">
-          
+          <img src={process.env.PUBLIC_URL + '../images/bg.png' } alt="bg" />
         </div>
       </div>
 
+      <Card/>
 
       </ >
   );
--- landing/src/components/Card.js
+++ landing/src/components/Card.js
@@ -1,8 +1,30 @@
 import React from 'react'
+import {IoMailOutline, IoChevronForward, IoApps, IoNotifications, IoPieChart,IoNewspaper,IoCard,IoColorFill, IoChevronForwardCircle} from 'react-icons/io5'
+import {IconContext} from 'react-icons'
 
 function Card () {
   return (
-    <div>Card</div>
+    <div className="service_container">
+      <div className="title_wrapper">
+        <span className="service_title">Our services</span>
+        <h2>save time managing social media <br />For your business</h2>
+      </div>
+      <div className="service_card">
+        <div className="card">
+          <span className="service_icon" style={{backgroundColor:"ddfbf9"}}>
+            <IconContext.Provider value={{ color: "#14da8f", size: "22px" }}>
+              <IoMailOutline/>
+            </IconContext.Provider>
+          </span>
+          <a href="#">
+            <span>learn more</span>
+            <IconContext.Provider value={{ color: "#14da8f", size: "18px" }}>
+              <IoChevronForward/>
+            </IconContext.Provider>
+          </a>
+        </div>
+      </div>
+    </div>
   )
 }
 
